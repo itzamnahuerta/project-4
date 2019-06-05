fd7aa37dc974f94840c1395518525229c164aa67
Itzamna <huerta.ek@gmail.com>
2019-06-05 10:28:15 -0400

    folder structure change

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use('/app', AppRouter);
-app.use('/auth', AuthRouter);
+// app.use('/app', AppRouter);
+// app.use('/auth', AuthRouter);
 app.use(passport.initialize());
 
 
-app.get("/hello", async (request, response) => {
+app.get("/", async (request, response) => {
     response.send("hello world");
 })
 
