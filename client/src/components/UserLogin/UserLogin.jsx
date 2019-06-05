1c0c9323be4b6d724c07734f1cd25491699a20f7
Itzamna <huerta.ek@gmail.com>
2019-06-05 11:14:57 -0400

    added service  folder and empty files

--- client/src/components/UserLogin/UserLogin.jsx
+++ client/src/components/UserLogin/UserLogin.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 
 export default class UserLogin extends Component {
+
     render() {
         return (
-            <div>
-                
+            <div className="log-in">
+                <div id="log-in-text"> login </div>
             </div>
         )
     }
